9f77a13bbf65c52d9f9d8a7dc0da63de6798da7d
Marium-sultana <mariumsultana073@gmail.com>
2021-08-16 09:18:34 +0600

    Initial Commit

---
+++ banking.js
@@ -0,0 +1,21 @@
+// console.log('added')
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-amount');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    console.log(depositAmount);
+    //get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+    // console.log(depositTotalText)
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount
+    //clear input field
+    depositInput.value = '';
+})
